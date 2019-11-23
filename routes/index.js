dafe1e36ef155f8d2c85dc97ba207b0d21064c22
Aditya Kumar Jha <adityajha126@gmail.com>
2019-11-23 12:26:53 +0530

    --

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,39 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const Url = require("../models/Url");
-const Store = require("../models/Shop");
+const Url = require('../models/Url');
+const Store = require('../models/Shop');
 
 // @route     GET /:code
 // @desc      Redirect to long/original URL
-router.get("/:code", async (req, res) => {
-  try {
-    const url = await Url.findOne({ urlCode: req.params.code });
+router.get('/:code', async (req, res) => {
+	try {
+		const url = await Url.findOne({ urlCode: req.params.code });
 
-    if (url) {
-      Store.updateOne(
-        { "test.id": 1 },
-        {
-          $set: {
-            "orders.$.followUp": 6
-          }
-        },
-        function(err, data) {
-          if (!err) {
-            console.log(data);
-          } else {
-            console.log(err);
-          }
-        }
-      );
-      return res.redirect(url.longUrl);
-    } else {
-      return res.status(404).json("No url found");
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json("Server error");
-  }
+		// if (url) {
+		Store.updateOne(
+			{ 'test.id': 1 },
+			{
+				$set: {
+					'test.$.followUp': 6
+				}
+			},
+			function(err, data) {
+				if (!err) {
+					console.log(data);
+				} else {
+					console.log(err);
+				}
+			}
+		);
+		return res.redirect(url.longUrl);
+		// } else {
+		//   return res.status(404).json("No url found");
+		// }
+	} catch (err) {
+		console.error(err);
+		res.status(500).json('Server error');
+	}
 });
 
 module.exports = router;
